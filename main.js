7344ba65159f44e0fac437071fc76d99b2e78b15
ProjectAGM <37554734+ProjectAGM@users.noreply.github.com>
2019-02-07 18:02:20 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 function chatHook(text) {
   var commandText = text.split(" ");
-  if(commandText[0] == ".heal") {
+  if(commandText[0] == ".wood") {
+    preventDefault();
+    Player.addItemInventory(17,64,0);
+  }
+  if(commandText[0] == ".iron") {
     preventDefault();
     clientMessage("You used the .heal command!");
   }
